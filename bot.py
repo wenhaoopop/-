27d9926c6e6c56d95adb0b9969b65c3b1467705a
wenhaoopop <130762174+wenhaoopop@users.noreply.github.com>
2023-04-14 20:29:00 +0800

    bot.py
    bot.py

---
+++ bot.py
@@ -0,0 +1,446 @@
+import telegram
+from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+# 机器人 token，需要在 BotFather 处获得
+TOKEN = "your_token_here"
+
+# 建立 Telegram Bot
+bot = telegram.Bot(token=TOKEN)
+
+# 建立更新器
+updater = Updater(token=TOKEN, use_context=True)
+
+# 用于记录已绑定地址的字典，key 为用户 ID，value 为地址列表
+address_dict = {}
+
+# 监听地址变化的函数
+def check_address():
+    # TODO: 实现监听地址变化的代码
+# 建立 start 命令处理函数
+def start(update: Update, context: CallbackContext) -> None:
+    user = update.message.from_user
+    # 欢迎信息
+    welcome_message = f"欢迎使用 USDT 监控机器人，{user.first_name}！\n\n"
+    # 如果用户已经绑定了地址，显示绑定信息，否则显示未绑定信息
+    if update.message.from_user.id in address_dict:
+        address_list = "\n".join(address_dict[update.message.from_user.id])
+        reply_message = f"{welcome_message}您已绑定的地址为：\n{address_list}"
+    else:
+        reply_message = f"{welcome_message}您还未绑定任何地址，请点击“添加监控”按钮绑定地址。"
+    # 发送欢迎信息
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+
+# 建立关注频道命令处理函数
+def follow_channel(update: Update, context: CallbackContext) -> None:
+    # 让用户关注指定频道
+    context.bot.send_message(chat_id=update.effective_chat.id, text="请关注 https://t.me/DiBaiLianMeng_ALLIANCE 频道后，输入 /bind_address 命令绑定地址。")
+
+# 建立绑定地址命令处理函数
+def bind_address(update: Update, context: CallbackContext) -> None:
+    # 检查用户是否已经关注指定频道
+    if not bot.get_chat_member(chat_id='DiBaiLianMeng_ALLIANCE', user_id=update.message.from_user.id).status == 'member':
+        context.bot.send_message(chat_id=update.effective_chat.id, text="请先关注 https://t.me/DiBaiLianMeng_ALLIANCE 频道再绑定地址。")
+        return
+
+    # 获取用户输入的地址
+    address = update.message.text.replace('/bind_address', '').strip()
+    # 判断地址是否合法
+    # TODO: 实现判断地址合法性的代码
+    if not is_valid_address(address):
+        context.bot.send_message(chat_id=update.effective_chat.id, text="地址格式不正确，请输入正确的地址。")
+        return
+
+    # 判断用户是否已经绑定过该地址
+    user_id = update.message.from_user.id
+    if user_id in address_dict and address in address_dict[user_id]:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="您已经绑定过该地址了。")
+        return
+
+    # 将地址添加到字典中
+    if user_id not in address_dict:
+        address_dict[user_id] = []
+    address_dict[user_id].append(address)
+
+    # 发送绑定成功信息
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"地址 {address} 绑定成功。")
+
+# 建立查询监控列表命令处理函数
+def list_address(update: Update, context: CallbackContext) -> None:
+    # 判断用户是否已经绑定了地址
+    if update.message.from_user.id not in address_dict:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="您还未绑定任何地址，请点击“添加监控”按钮绑定地址。")
+        return
+
+    # 获取用户已经绑定的地址列表
+    address_list = "\n".join(address_dict[update.message.from_user.id])
+    # 发送地址列表
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"您已经绑定的地址为：\n{address_list}")
+
+# 建立处理未知命令的处理函数
+def unknown(update: Update, context: CallbackContext) -> None:
+    # 发送帮助信息
+    context.bot.send_message(chat_id=update.effective_chat.id, text="您输入的命令不正确，请点击下方按钮选择命令。", reply_markup=main_keyboard())
+
+# 定义主菜单键盘
+def main_keyboard() -> ReplyKeyboardMarkup:
+    keyboard = [
+        [telegram.KeyboardButton("添加监控")],
+        [telegram.KeyboardButton("删除监控"), telegram.KeyboardButton("监控列表")],
+        [telegram.KeyboardButton("查询明细"), telegram.KeyboardButton("开通会员"), telegram.KeyboardButton("联系客服")],
+    ]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+# 建立命令处理器
+start_handler = CommandHandler('start', start)
+follow_channel_handler = CommandHandler('follow_channel', follow_channel)
+bind_address_handler = CommandHandler('bind_address', bind_address)
+list_address_handler = CommandHandler('list_address', list_address)
+unknown_handler = MessageHandler(Filters.command, unknown)
+
+# 添加命令处理器
+updater.dispatcher.add_handler(start_handler)
+updater.dispatcher.add_handler(follow_channel_handler)
+updater.dispatcher.add_handler(bind_address_handler)
+updater.dispatcher.add_handler(list_address_handler)
+updater.dispatcher.add_handler(unknown_handler)
+   
+# 建立删除监控命令处理函数
+def delete_address(update: Update, context: CallbackContext) -> None:
+    # 判断用户是否已经绑定了地址
+    if update.message.from_user.id not in address_dict:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="您还未绑定任何地址，请点击“添加监控”按钮绑定地址。")
+        return
+
+    # 获取用户已经绑定的地址列表
+    address_list = address_dict[update.message.from_user.id]
+    # 如果用户只绑定了一个地址，则直接删除该地址
+    if len(address_list) == 1:
+        del address_dict[update.message.from_user.id]
+    else:
+        # 显示用户绑定的地址列表
+        address_str = "\n".join([f"{i + 1}. {addr}" for i, addr in enumerate(address_list)])
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"您已经绑定的地址为：\n{address_str}\n请输入要删除的地址序号：")
+
+        # 等待用户输入要删除的地址序号
+        context.user_data['delete_address'] = True
+        context.user_data['address_list'] = address_list
+        return
+
+# 建立查询明细命令处理函数
+def query_transactions(update: Update, context: CallbackContext) -> None:
+    # 获取用户输入的地址
+    address = update.message.text.replace('/query_transactions', '').strip()
+    # 判断地址是否合法
+    # TODO: 实现判断地址合法性的代码
+    if not is_valid_address(address):
+        context.bot.send_message(chat_id=update.effective_chat.id, text="地址格式不正确，请输入正确的地址。")
+        return
+
+    # 获取地址的网络类型
+    network_type = get_network_type(address)
+    # 如果网络类型不支持，则提示用户
+    if network_type not in ["TRC20", "ERC20", "BSC", "POLYGON", "OKC"]:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="该网络不支持 USDT 监控。")
+        return
+
+    # 获取地址的转入转出记录
+    transactions = get_transactions(address, network_type)
+    if not transactions:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="该地址最近没有 USDT 转账记录。")
+        return
+
+    # 获取最近的 10 条转入转出记录
+    transaction_str_list = []
+    for i, tx in enumerate(transactions[:10]):
+        transaction_type = "转入" if tx['type'] == 'in' else "转出"
+        transaction_amount = tx['amount']
+        transaction_time = tx['time']
+        transaction_str = f"{i+1}. {transaction_type} {transaction_amount} USDT，时间：{transaction_time}"
+        transaction_str_list.append(transaction_str)
+
+    # 发送转入转出记录
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"地址 {address} 最近的 10 条转入转出记录为：\n{'\n'.join(transaction_str_list
+# 建立开通会员命令处理函数
+def become_member(update: Update, context: CallbackContext) -> None:
+    # TODO: 实现开通会员功能的代码
+    context.bot.send_message(chat_id=update.effective_chat.id, text="该功能正在开发中，敬请期待。")
+
+# 建立联系客服命令处理函数
+def contact_service(update: Update, context: CallbackContext) -> None:
+    # 发送联系客服信息
+    context.bot.send_message(chat_id=update.effective_chat.id, text="如有任何问题，请联系客服：\n邮箱：service@usdtmonitor.com\n微信：usdtmonitor")
+
+# 建立回复消息处理函数
+def reply_message(update: Update, context: CallbackContext) -> None:
+    # 如果用户正在删除监控，处理用户输入的地址序号
+    if 'delete_address' in context.user_data and context.user_data['delete_address']:
+        # 获取用户输入的地址序号
+        try:
+            index = int(update.message.text.strip()) - 1
+        except ValueError:
+            context.bot.send_message(chat_id=update.effective_chat.id, text="请输入正确的地址序号。")
+            return
+
+        # 判断地址序号是否正确
+        address_list = context.user_data['address_list']
+        if index < 0 or index >= len(address_list):
+            context.bot.send_message(chat_id=update.effective_chat.id, text="请输入正确的地址序号。")
+            return
+
+        # 删除地址并发送删除成功信息
+        address_dict[update.message.from_user.id].pop(index)
+        context.bot.send_message(chat_id=update.effective_chat.id, text="地址删除成功。")
+
+        # 将删除监控标志设为 False
+        context.user_data['delete_address'] = False
+        context.user_data['address_list'] = None
+        return
+
+# 建立消息处理器
+message_handler = MessageHandler(Filters.text, reply_message)
+
+# 添加消息处理器
+updater.dispatcher.add_handler(message_handler)
+
+# 启动机器人
+updater.start_polling()
+updater.idle()
+import os
+import telegram
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+
+# 定义全局变量
+address_dict = {}
+
+# 建立 start 命令处理函数
+def start(update: Update, context: CallbackContext) -> None:
+    # 发送欢迎信息和主菜单
+    context.bot.send_message(chat_id=update.effective_chat.id, text="欢迎使用 USDT 监控机器人！", reply_markup=main_keyboard())
+
+# 建立 follow_channel 命令处理函数
+def follow_channel(update: Update, context: CallbackContext) -> None:
+    # 发送关注频道信息
+    context.bot.send_message(chat_id=update.effective_chat.id, text="请关注 https://t.me/DiBaiLianMeng_ALLIANCE 频道并联系客服绑定地址。")
+
+# 建立 bind_address 命令处理函数
+def bind_address(update: Update, context: CallbackContext) -> None:
+    # 获取用户输入的地址
+    address = update.message.text.replace('/bind_address', '').strip()
+    # 判断地址是否合法
+    # TODO: 实现判断地址合法性的代码
+    if not is_valid_address(address):
+        context.bot.send_message(chat_id=update.effective_chat.id, text="地址格式不正确，请输入正确的地址。")
+        return
+
+    # 判断用户是否已经绑定过该地址
+    user_id = update.message.from_user.id
+    if user_id in address_dict and address in address_dict[user_id]:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="您已经绑定过该地址了。")
+        return
+
+    # 将地址添加到字典中
+    if user_id not in address_dict:
+        address_dict[user_id] = []
+    address_dict[user_id].append(address)
+
+    # 发送绑定成功信息
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"地址 {address} 绑定成功。")
+
+# 建立查询监控列表命令处理函数
+def list_address(update: Update, context: CallbackContext) -> None:
+    # 判断用户是否已经绑定了地址
+    if update.message.from_user.id not in address_dict:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="您还未绑定任何地址，请点击“添加监控”按钮绑定地址。")
+        return
+
+    # 获取用户已经绑定的地址列表
+    address_list = "\n".join(address_dict[update.message.from_user.id])
+    # 发送地址列表
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"您已经绑定的地址为：\n{address_list}")
+
+# 建立删除监控命令处理函数
+def delete_address(update: Update, context: CallbackContext) -> None:
+    # 判断用户是否已经绑定了地址
+    if update.message.from_user.id not in address_dict:
+        context.bot.send_message(chat
+# 建立删除监控命令处理函数
+def delete_address(update: Update, context: CallbackContext) -> None:
+    # 判断用户是否已经绑定了地址
+    if update.message.from_user.id not in address_dict:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="您还未绑定任何地址，请点击“添加监控”按钮绑定地址。")
+        return
+
+    # 获取用户已经绑定的地址列表
+    address_list = address_dict[update.message.from_user.id]
+
+    # 判断用户输入的地址是否存在
+    address = update.message.text.replace('/delete_address', '').strip()
+    if address not in address_list:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="您未绑定该地址，请输入正确的地址。")
+        return
+
+    # 从字典中删除地址
+    address_list.remove(address)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"地址 {address} 删除成功。")
+
+# 建立查询明细命令处理函数
+def detail(update: Update, context: CallbackContext) -> None:
+    # 获取用户输入的地址
+    address = update.message.text.replace('/detail', '').strip()
+    # 判断地址是否合法
+    # TODO: 实现判断地址合法性的代码
+    if not is_valid_address(address):
+        context.bot.send_message(chat_id=update.effective_chat.id, text="地址格式不正确，请输入正确的地址。")
+        return
+
+    # 获取地址的交易记录
+    tx_list = get_tx_list(address)
+    # 只保留 USDT 的转账记录
+    usdt_tx_list = [tx for tx in tx_list if tx['tokenSymbol'] == 'USDT']
+    # 取最近的 10 条记录
+    usdt_tx_list = usdt_tx_list[:10]
+    # 格式化交易记录信息
+    detail_list = [f"交易哈希：{tx['txhash']}\n时间：{tx['timeStamp']}\n数额：{tx['value']} {tx['tokenSymbol']}" for tx in usdt_tx_list]
+    detail_info = "\n\n".join(detail_list)
+    # 发送交易记录信息
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"{address} 最近 10 条 USDT 转账记录：\n\n{detail_info}")
+
+# 建立开通会员命令处理函数
+def become_vip(update: Update, context: CallbackContext) -> None:
+    # 发送开通会员信息
+    context.bot.send_message(chat_id=update.effective_chat.id, text="请联系客服开通会员。")
+
+# 建立联系客服命令处理函数
+def contact_service(update: Update, context: CallbackContext) -> None:
+    # 发送联系客服信息
+    context.bot.send_message(chat_id=update.effective_chat.id, text="请联系客服：\n电话：xxx-xxxxxxx\n邮箱：xxxxx@xxx.com\n微信：xxxxxxxxxxxx")
+
+# 建立消息处理函数
+def message_handler(update: Update, context: CallbackContext) -> None:
+    # 判断是否是按钮事件
+    if update.callback_query:
+        # 处理按钮事件
+# 建立消息处理函数
+def message_handler(update: Update, context: CallbackContext) -> None:
+    # 判断是否是按钮事件
+    if update.callback_query:
+        # 处理按钮事件
+        query = update.callback_query
+        query.answer()
+        # 根据按钮的 data 值调用相应的函数
+        if query.data == 'add':
+            add_monitor(query)
+        elif query.data == 'delete':
+            delete_monitor(query)
+        elif query.data == 'list':
+            list_monitor(query)
+        elif query.data == 'detail':
+            query.message.reply_text('请输入要查询的地址：')
+        elif query.data == 'vip':
+            become_vip(query)
+        elif query.data == 'contact':
+            contact_service(query)
+        return
+
+    # 判断是否是命令消息
+    if update.message and update.message.text.startswith('/'):
+        command = update.message.text.split()[0].lower()
+        # 根据命令调用相应的函数
+        if command == '/start':
+            start(update, context)
+        elif command == '/follow_channel':
+            follow_channel(update, context)
+        elif command == '/bind_address':
+            bind_address(update, context)
+        elif command == '/list_address':
+            list_address(update, context)
+        elif command == '/delete_address':
+            delete_address(update, context)
+        elif command == '/detail':
+            detail(update, context)
+        elif command == '/become_vip':
+            become_vip(update, context)
+        elif command == '/contact_service':
+            contact_service(update, context)
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id, text="无效的命令，请重新输入。")
+
+# 建立添加监控函数
+def add_monitor(query) -> None:
+    # 获取用户输入的地址
+    query.message.reply_text('请输入要添加的地址：')
+    # 为地址添加监控
+    # TODO: 实现添加监控的代码
+
+# 建立删除监控函数
+def delete_monitor(query) -> None:
+    # 获取用户输入的地址
+    query.message.reply_text('请输入要删除的地址：')
+    # 为地址删除监控
+    # TODO: 实现删除监控的代码
+
+# 建立查询监控列表函数
+def list_monitor(query) -> None:
+    # 获取用户已经绑定的地址列表
+    if query.from_user.id not in address_dict:
+        query.message.reply_text("您还未绑定任何地址，请点击“添加监控”按钮绑定地址。")
+        return
+
+    address_list = address_dict[query.from_user.id]
+
+    # 发送地址列表
+    if address_list:
+        address_markup = ReplyKeyboardMarkup([[address] for address in address_list], resize_keyboard=True, one_time_keyboard=True)
+        query.message.reply_text("请选择要查询的地址：", reply_markup=address_markup)
+    else:
+        query.message.reply_text("您还未绑定任何地址，请点击“添加监控”按钮绑定地址。")
+
+# 建立查询 USDT 交易记录函数
+def get_tx_list(address: str) -> list:
+    # 根据地址获取 USDT 交易记录
+    # TODO: 实现获取交易记录的代码
+
+# 建立判断地址合法性函数
+def is_valid_address(address: str) -> bool:
+    # 判断地址
+# 建立判断地址合法性函数
+def is_valid_address(address: str) -> bool:
+    # 判断地址是否合法
+    # TODO: 实现判断地址合法性的代码
+
+# 建立主菜单函数
+def main_keyboard() -> ReplyKeyboardMarkup:
+    # 定义主菜单按钮
+    button_list = [
+        ['添加监控', '删除监控'],
+        ['监控列表', '查询明细'],
+        ['开通会员', '联系客服']
+    ]
+    # 创建主菜单键盘
+    main_markup = ReplyKeyboardMarkup(button_list, resize_keyboard=True)
+    return main_markup
+
+# 建立电报机器人实例
+bot_token = os.environ['BOT_TOKEN']
+updater = Updater(token=bot_token, use_context=True)
+
+# 建立命令处理函数
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('follow_channel', follow_channel))
+updater.dispatcher.add_handler(CommandHandler('bind_address', bind_address))
+updater.dispatcher.add_handler(CommandHandler('list_address', list_address))
+updater.dispatcher.add_handler(CommandHandler('delete_address', delete_address))
+updater.dispatcher.add_handler(CommandHandler('detail', detail))
+updater.dispatcher.add_handler(CommandHandler('become_vip', become_vip))
+updater.dispatcher.add_handler(CommandHandler('contact_service', contact_service))
+
+# 建立消息处理函数
+updater.dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
+
+# 启动电报机器人
+updater.start_polling()
+updater.idle()
+
+请你帮我检查这个代码
